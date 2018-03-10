101bcb9feca1e9265f014113d1de277a0def4f1e
Vladislav <vkiytkin@yandex.ru>
2018-03-10 16:34:51 +0300

    Deleted Key

--- Programs/Yandex.Translate for ComputerCraft.lua
+++ Programs/Yandex.Translate for ComputerCraft.lua
@@ -1,7 +1,7 @@
 -- ЭТО ГОВНО НЕ РАБОТАЕТ. ОНО КАК БЫ РАБОТАЕТ, НО ЗАДАВАТЬ ПЕРЕМЕННЫЕ НА РУССКОМ ЧЕРЕЗ CompCraft НЕЛЬЗЯ 
 
-os.loЗЯ НА adAPI("json")
-local APIkey = "trnsl.1.1.20171125T112911Z.8e1a92701f1beee6.9817d9f86ff90c27fc12a1192a0c736bd20fcfec"
+os.loadAPI("json")
+local APIkey = "  "
 
 str = http.get("https://translate.yandex.net/api/v1.5/tr.json/translate?key=".. APIkey .."&text=Насос&lang=ru-en&format=plain").readAll()
 obj = json.decode(str)
